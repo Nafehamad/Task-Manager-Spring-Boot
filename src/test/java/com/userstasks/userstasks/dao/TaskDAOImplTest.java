02fde0db569c38e71541b6a24b09db33c0be9c48
nafe hamad <nafehamad90@gmail.com>
2020-07-15 12:05:05 +0300

    fourth commit

--- src/test/java/com/userstasks/userstasks/dao/TaskDAOImplTest.java
+++ src/test/java/com/userstasks/userstasks/dao/TaskDAOImplTest.java
@@ -3,14 +3,10 @@ package com.userstasks.userstasks.dao;
 
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import com.userstasks.userstasks.dao.UserDAOImpl;
 import com.userstasks.userstasks.entity.Task;
-import com.userstasks.userstasks.entity.User;
 import org.hibernate.Session;
 import org.junit.Assert;
 import org.junit.Test;
@@ -18,10 +14,8 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
+import org.hibernate.query.Query;
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaQuery;
 import java.util.*;
@@ -32,7 +26,7 @@ public class TaskDAOImplTest {
     @Mock
     EntityManager entityManager;
     @Mock
-    Query query;
+    Query <Task> query;
     @Mock
     CriteriaQuery criteriaQuery;
     @Mock
@@ -50,9 +44,12 @@ public class TaskDAOImplTest {
    public void findById() {
 
         Task task=new Task(1,"one",false);
-        when(entityManager.createQuery(any(String.class), eq(Task.class))).thenReturn(query1);
-        when(query1.setParameter(any(String.class), any(String.class))).thenReturn(query1);
-        when(query1.getSingleResult()).thenReturn(task);
+        when(entityManager.unwrap(Session.class)).thenReturn(session);
+        when(session.get(Task.class,1)).thenReturn(task);
+        taskDAOImpl.findById(1);
+        assertEquals(taskDAOImpl.findById(1).getDescription(),"one");
+        assertEquals(taskDAOImpl.findById(1).isCompleted(),false);
+
 
     }
 
@@ -60,26 +57,34 @@ public class TaskDAOImplTest {
   public   void findAll() {
 
         Task task=new Task(1,"one",false);
-        when(entityManager.createQuery(any(String.class), eq(Task.class))).thenReturn(query1);
-        when(query1.setParameter(any(String.class), any(String.class))).thenReturn(query1);
-        when(query1.getResultList()).thenReturn(taskList);
+        Task task1=new Task(2,"one",false);
+        Task task2=new Task(3,"one",false);
+        taskList.add(task);
+        taskList.add(task);
+        when(entityManager.unwrap(Session.class)).thenReturn(session);
+        when(session.createQuery(any(String.class), eq(Task.class))).thenReturn(query);
+        when(query.setParameter(any(String.class), any(String.class))).thenReturn(query);
+        when(query.getResultList()).thenReturn(taskList);
+        Assert.assertEquals(taskDAOImpl.findAll().size(),3);
     }
 
     @Test
   public  void save() {
 
-        Task task=new Task();
-        taskDAO.save(task);
-        when(taskDAO.save(task)).thenReturn(task);
+        Task task=new Task(1,"one",false);
+        when(entityManager.unwrap(Session.class)).thenReturn(session);
+        taskDAOImpl.save(task);
     }
 
     @Test
    public void deleteById() {
 
         int id=1;
-        Task task=new Task();
-        when(entityManager.createQuery("delete from Task where id=:id")).thenReturn(query);
+        Task task=new Task(1,"one",false);
+        when(entityManager.unwrap(Session.class)).thenReturn(session);
+        when(session.createQuery("delete from Task where id=:id")).thenReturn(query);
         when(query.executeUpdate()).thenReturn(1);
+        taskDAOImpl.deleteById(id);
 
     }
 }
--- src/test/java/com/userstasks/userstasks/dao/UserDAOImplTest.java
+++ src/test/java/com/userstasks/userstasks/dao/UserDAOImplTest.java
@@ -3,34 +3,22 @@ package com.userstasks.userstasks.dao;
 
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyString;
-
-import com.userstasks.userstasks.dao.UserDAOImpl;
-import com.userstasks.userstasks.entity.Task;
 import com.userstasks.userstasks.entity.User;
 import org.hibernate.Session;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.*;
-
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaQuery;
-import java.io.Serializable;
 import java.util.*;
-
+import org.hibernate.query.Query;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserDAOImplTest {
@@ -40,7 +28,7 @@ public class UserDAOImplTest {
     @Mock
     EntityManager entityManager;
     @Mock
-    Query query;
+    Query<User> query;
     @Mock
     CriteriaQuery criteriaQuery;
     @Mock
@@ -61,9 +49,13 @@ public class UserDAOImplTest {
     public void findById() {
 
           User user=new User("a","a@a","a",10);
-          when(entityManager.createQuery(any(String.class), eq(User.class))).thenReturn(query1);
-          when(query1.setParameter(any(String.class), any(String.class))).thenReturn(query1);
-          when(query1.getSingleResult()).thenReturn(user);
+          when(entityManager.unwrap(Session.class)).thenReturn(session);
+          when(session.get(User.class,"a@a")).thenReturn(user);
+          userDAOImpl.findById("a@a");
+          assertEquals(userDAOImpl.findById("a@a").getName(),"a");
+          assertEquals(userDAOImpl.findById("a@a").getPassword(),"a");
+          assertEquals(userDAOImpl.findById("a@a").getAge(),10);
+
 
 
     }
@@ -72,28 +64,34 @@ public class UserDAOImplTest {
    public void findAll() {
 
        User user=new User("a","a@a","a",10);
-       when(entityManager.createQuery(any(String.class), eq(User.class))).thenReturn(query1);
-       when(query1.setParameter(any(String.class), any(String.class))).thenReturn(query1);
-       when(query1.getResultList()).thenReturn(userList);
+       User user1=new User("","a@a","a",10);
+       userList.add(user);
+       userList.add(user1);
+       when(entityManager.unwrap(Session.class)).thenReturn(session);
+       when(session.createQuery(any(String.class), eq(User.class))).thenReturn(query);
+       when(query.setParameter(any(String.class), any(String.class))).thenReturn(query);
+       when(query.getResultList()).thenReturn(userList);
+       Assert.assertEquals(userDAOImpl.findAll().size(),2);
 
     }
 
    @Test
   public void save() {
 
-       User user1=new User();
-       userDAO.save(user1);
-       when(userDAO.save(user1)).thenReturn(user1);
-
+       User user=new User("a","a@a","a",10);
+       when(entityManager.unwrap(Session.class)).thenReturn(session);
+       userDAOImpl.save(user);
 
      }
 
     @Test
    public void deleteById() {
         String id="a@a";
-        User user=new User();
-        when(entityManager.createQuery("delete from User where email=:id")).thenReturn(query);
+        User user=new User("a",id,"a",10);
+        when(entityManager.unwrap(Session.class)).thenReturn(session);
+        when(session.createQuery("delete from User where email=:id")).thenReturn(query);
         when(query.executeUpdate()).thenReturn(1);
+        userDAOImpl.deleteById(id);
 
 
     }
